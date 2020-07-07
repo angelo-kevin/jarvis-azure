32dbd69ab80b37def06a90382334429ca38d7d99
Kevin Angelo <kevinn.angelo@gmail.com>
2020-07-08 05:40:55 +0700

    added user flow

--- bots/suggestedActionsBot.js
+++ bots/suggestedActionsBot.js
@@ -33,83 +33,85 @@ class SuggestedActionsBot extends ActivityHandler {
 
             // If the `text` is in the Array, a valid color was selected and send agreement.
             if (validMethod.includes(text)) {
-                await context.sendActivity(`You chose ${ text }.`);
+                await context.sendActivity(`Sure, let's do delivery from Store A, your approximate waiting time would be 40 minutes, sounds good?`);
+
+                var temp = "29";
+
+                const opt = {
+                    uri: tempUri,
+                    method: "GET"
+                }
+
+                req(opt, (err, res, body) => {
+                    if (!err && res.statusCode == 200) {
+                        var obj = JSON.parse(body);
+                        temp = "" + obj.temperature.data[5].value;
+                    } else {
+                        console.log("The request failed with status code: " + res.statusCode);
+                    }
+                });
+
+                var dine = "0", take = "0", deliv = "0";
+
+                if (text.includes("Dine")){
+                    dine = "1";
+                } else if (text.includes("Take")){
+                    take = "1";
+                } else{
+                    deliv = "1";
+                }
+
+                let data =  {
+                    "Inputs": {
+                        "input1":
+                        {
+                            "ColumnNames": ["product_desc", "qty", "is_member", "Dine In", "Take Away", "Delivery", "Region", "temp", "order_time"],
+                            "Values": [ [ "value", "1", "1", dine, take, deliv, "Tai Po", temp, "" ], [ "value", "0", "0", "0", "0", "0", "value", "0", "" ], ]
+                        },
+                    },
+                    "GlobalParameters": {}
+                }
+
+                const options = {
+                    uri: uri,
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + apiKey,
+                    },
+                    body: JSON.stringify(data)
+                }
+
+                var food = "Supreme";
+
+                req(options, (err, res, body) => {
+                    if (!err && res.statusCode == 200) {
+                        var obj = JSON.parse(body);
+                        food = obj.Results.output1.value.Values[0][14];
+                    } else {
+                        console.log("The request failed with status code: " + res.statusCode);
+                    }
+                });
+
+                await delay(10000);
+
+                await this.foodSuggestion(context, food);
+
+                await delay(10000);
+
+                await context.sendActivity("Cool! Let me deliver that to you.")
+
+                await delay(10000);
+
+                await this.loving(context);
+
+                await delay(10000);
+
+                await this.feedback(context);
+
             } else if (validNum.includes(text)){
                 await context.sendActivity('Thanks for the feedback. Have a nice day, Kevin :)')
-            } else{
-                await context.sendActivity('Please select a valid option.');
             }
-
-            var temp = "0";
-
-            const opt = {
-                uri: tempUri,
-                method: "GET"
-            }
-
-            req(opt, (err, res, body) => {
-                if (!err && res.statusCode == 200) {
-                    var obj = JSON.parse(body);
-                    temp = "" + obj.temperature.data[5].value;
-                } else {
-                    console.log("The request failed with status code: " + res.statusCode);
-                }
-            });
-
-            await delay(1000);
-
-            var dine = "0", take = "0", deliv = "0";
-
-            if (text.includes("Dine")){
-                dine = "1";
-            } else if (text.includes("Take")){
-                take = "1";
-            } else{
-                deliv = "1";
-            }
-
-            let data =  {
-                "Inputs": {
-                    "input1":
-                    {
-                        "ColumnNames": ["product_desc", "qty", "is_member", "Dine In", "Take Away", "Delivery", "Region", "temp", "order_time"],
-                        "Values": [ [ "value", "1", "1", dine, take, deliv, "Tai Po", temp, "" ], [ "value", "0", "0", "0", "0", "0", "value", "0", "" ], ]
-                    },
-                },
-                "GlobalParameters": {}
-            }
-
-            const options = {
-                uri: uri,
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer " + apiKey,
-                },
-                body: JSON.stringify(data)
-            }
-
-            var food = "";
-
-            req(options, (err, res, body) => {
-                if (!err && res.statusCode == 200) {
-                    var obj = JSON.parse(body);
-                    food = obj.Results.output1.value.Values[0][14];
-                } else {
-                    console.log("The request failed with status code: " + res.statusCode);
-                }
-            });
-
-            await delay(3500);
-
-            // After the bot has responded send the suggested actions.
-            await this.foodSuggestion(context, food);
-
-            await delay(2000);
-
-            await this.loving(context);
-
-            await this.feedback(context);
 
             // By calling next() you ensure that the next BotHandler is run.
             await next();
@@ -143,12 +145,12 @@ class SuggestedActionsBot extends ActivityHandler {
     }
 
     async foodSuggestion(turnContext, food) {
-        var reply = `I recommend you having ${ food }.`;
+        var reply = `${ food } seems nice, wanna try that?`;
         await turnContext.sendActivity(reply);
     }
 
     async loving(turnContext) {
-        var reply = 'Enjoying your food, Kevin?';
+        var reply = 'Enjoying your food, Kevin? Care to give us some feedbacks on this recommendation?';
         await turnContext.sendActivity(reply);
     }
 
